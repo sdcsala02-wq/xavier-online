b501d6e8e8d9ff47c4d42c6ba3b36656109e186f
Edinho Souza <fisio.x97@gmail.com>
2026-07-01 14:52:28 -0300

    Corrige SSL do PostgreSQL Railway

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: false
-});
-
+ssl: {
+  rejectUnauthorized: false
+}
 module.exports = pool;
