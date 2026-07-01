f864e52acbed404e48e5c1528827346218b0a9ec
Edinho Souza <fisio.x97@gmail.com>
2026-07-01 15:26:46 -0300

    Volta configuracao anterior do banco

--- backend/db.js
+++ backend/db.js
@@ -9,6 +9,10 @@ const { Pool } = require("pg");
 
 require("dotenv").config();
 
+const { Pool } = require("pg");
+
+require("dotenv").config();
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: false
